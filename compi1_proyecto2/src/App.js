35e1cee5fafb6a35ca6ff1ca4bd246f09353b0a4
Chocs <Luisitom@gmail.com>
2024-03-26 23:01:22 -0600

    Frontend

---
+++ compi1_proyecto2/src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+      </header>
+      {/* Renderiza el componente TextAreaWithButton */}
+      <TextAreaWithButton />
+    </div>
+  );
+}
+
+function TextAreaWithButton() {
+  const [text, setText] = useState('');
+  const [otherText, setOtherText] = useState('');
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    
+    reader.onload = function(event) {
+      setText(event.target.result);
+    };
+    
+    reader.readAsText(file);
+  };
+
+  const handlePrintText = () => {
+    console.log(text);
+  };
+
+  return (
+    <div className="container">
+      <textarea
+        className="textarea"
+
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Escribe o carga tu texto aquí..."
+      />
+      <textarea
+        className="textarea"
+        value={otherText}
+        onChange={(e) => setOtherText(e.target.value)}
+        placeholder="Otro textarea"
+      />
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handlePrintText}>Imprimir en Consola</button>
+      
+    </div>
+  );
+}
+
+
+
+export default App;
